390a4e19388733469c692b2b1d66999f018ccea9
shen chen <scv119@gmail.com>
2012-02-01 15:31:44 +0800

    new under stand of __new__ __init__ and metaclass

--- python_study/MetaClass.py
+++ python_study/MetaClass.py
@@ -1,18 +1,39 @@
 #!/bin/env python
+alist = []
 
-class My_Meta_Class(type):
-    def __init__(cls, name, bases, dct):
-        print cls, bases
-        super(My_Meta_class,cls).__init__(cls, name, bases, dct) 
+class MetaClass(type):
+    def __new__(meta, classname, bases, classDict):
+        alist.append(meta)
+        print 'Class Name:', classname
+        print 'Bases:', bases
+        print 'Class Attributes', classDict
+        classDict['method1'] = classDict['method'] 
+        return super(MetaClass, meta).__new__(meta, classname, bases, classDict)
 
 
-class C(object):
-    __meta_class__ = My_Meta_Class
+    def __init__(cls, classname, bases, classDict):
+        alist.append(cls)
+        print('wtf')
+        cls.method2 = classDict['method1']
+        super(MetaClass, cls).__init__(classname, bases, classDict)
+
+class Test(object):
+    __metaclass__ = MetaClass
     def __init__(self):
         pass
 
+    def __new__(cls):
+        return object.__new__(cls)
+
+    def method(self):
+        pass
 
 if __name__ == '__main__':
-    c = C()
-    C
+    print dir(Test)
+    test = Test()
+    test.method1()
+    print alist
+    print test.__init__
+    print Test.__new__
+    
 
